c83b6ed71a31aefc5ba7e9562e4ab5ff089a6122
Sebastian Gałan <sebastian.galan7625@gmail.com>
2025-01-28 10:17:47 +0100

    Created hero

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { AsideMenuComponent } from "./aside-menu/aside-menu.component";
+import { HeroComponent } from "./hero/hero.component";
 
 @Component({
   selector: 'app-home',
-  imports: [AsideMenuComponent],
+  imports: [AsideMenuComponent, HeroComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
