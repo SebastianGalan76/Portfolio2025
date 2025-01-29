e6b836cde372cbe46ef6e68791ae82603517cf9a
Sebastian Gałan <sebastian.galan7625@gmail.com>
2025-01-29 21:30:38 +0100

    Created profile section

---
+++ src/app/home/about-section/about-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SectionHeaderComponent } from "../section-header/section-header.component";
+
+@Component({
+  selector: 'app-about-section',
+  imports: [SectionHeaderComponent],
+  templateUrl: './about-section.component.html',
+  styleUrl: './about-section.component.scss'
+})
+export class AboutSectionComponent {
+
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,10 +4,11 @@ import { HeroComponent } from "./hero/hero.component";
 import { StackComponent } from "./stack-section/stack-section.component";
 import { ContactSectionComponent } from "./contact-section/contact-section.component";
 import { ProjectSectionComponent } from "./project-section/project-section.component";
+import { AboutSectionComponent } from "./about-section/about-section.component";
 
 @Component({
   selector: 'app-home',
-  imports: [AsideMenuComponent, HeroComponent, StackComponent, ContactSectionComponent, ProjectSectionComponent],
+  imports: [AsideMenuComponent, HeroComponent, StackComponent, ContactSectionComponent, ProjectSectionComponent, AboutSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
