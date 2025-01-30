e022155bf325e9fdd55c50a13ad9348e01d3b9c1
Sebastian Gałan <sebastian.galan7625@gmail.com>
2025-01-30 18:08:52 +0100

    Added preview image and description to projects

--- src/app/home/project-section/project-small/project-small.component.ts
+++ src/app/home/project-section/project-small/project-small.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+export interface GitHubLink {
+  name: string;
+  link: string;
+}
 
 @Component({
   selector: 'app-project-small',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './project-small.component.html',
   styleUrl: './project-small.component.scss'
 })
 export class ProjectSmallComponent {
+  @Input({ required: true }) imgSrc!: string;
+  @Input({ required: true }) name!: string;
+  @Input({ required: true }) type!: string;
+  @Input({ required: true }) description!: string;
 
+  @Input() github: GitHubLink[] = [];
+  @Input() live: string | null = null;
 }
