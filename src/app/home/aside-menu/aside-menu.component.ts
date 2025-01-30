e79590a0b647939e5743926d3b8338795387fcce
Sebastian Gałan <sebastian.galan7625@gmail.com>
2025-01-30 12:24:56 +0100

    Added header for small devices

--- src/app/home/aside-menu/aside-menu.component.ts
+++ src/app/home/aside-menu/aside-menu.component.ts
@@ -1,16 +1,24 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-aside-menu',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './aside-menu.component.html',
   styleUrl: './aside-menu.component.scss'
 })
 export class AsideMenuComponent {
+  isActive: boolean = false;
+
   scrollTo(elementId: string): void {
     const element = document.getElementById(elementId);
     if (element) {
       element.scrollIntoView({ behavior: 'smooth' });
+      this.isActive = false;
     }
+  }
+
+  toggleMenu() {
+    this.isActive = !this.isActive;
   }
 }
